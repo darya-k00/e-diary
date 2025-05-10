76e59e53c24e863897c055f78fe6e830aae6e541
darya-k00 <dasha.kon.060203@gmail.com>
2025-05-10 19:25:06 +0300

    Add files via upload

---
+++ scripts.py
@@ -0,0 +1,45 @@
+from datacenter.models import Schoolkid
+from datacenter.models import Mark
+from datacenter.models import Chastisement
+from datacenter.models import Lesson
+from datacenter.models import Commendation
+import random
+
+
+def fix_marks(schoolkid):
+    schoolkid = get_schoolkid(schoolkid)
+    if schoolkid is None:
+        return
+    bad_grades = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=5)
+    print('Плохие оценки исправлены.')
+    return
+
+
+def remove_chastisements(schoolkid):
+    schoolkid_chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
+    schoolkid_chastisements.delete()
+    print('Замечания удалены.')
+    return
+
+
+def create_commendation(schoolkid, subject_name):
+    lessons=Lesson.objects.filter(year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter)
+    lesson = lessons.filter(subject__title=subject_name).order_by('date').first()
+    commendation_texts = [
+        'Хвалю!',
+        'Молодец!',
+        'Сказано здорово - просто ясно!',
+        'Я поражен!',
+        'Ты растешь над собой!'
+    ]
+
+    Commendation.objects.create(schoolkid=schoolkid, teacher=lesson.teacher,subject=lesson.subject,created=lesson.date,text=random.choice(commendation_texts))
+
+
+def get_schoolkid(full_name):
+    try:
+        return Schoolkid.objects.get(full_name__contains=full_name)
+    except Schoolkid.ObjectDoesNotExist:
+        print("Такого ученика не существует")
+    except Schoolkid.MultipleObjectsReturned:
+        print("Нашёл два и более ученика")
